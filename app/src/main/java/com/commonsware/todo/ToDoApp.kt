1e95348a3362b59fb8c1eb5f3d3198b81cd75631
neillbarrett <barrettneill@gmail.com>
2023-11-22 13:05:56 -0800

    Completed T13-Inverting Our Dependencies

--- app/src/main/java/com/commonsware/todo/ToDoApp.kt
+++ app/src/main/java/com/commonsware/todo/ToDoApp.kt
@@ -1,6 +1,22 @@
 package com.commonsware.todo
 
 import android.app.Application
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.koin.dsl.module
 
 class ToDoApp : Application() {
+    private val koinModule = module {
+        single { ToDoRepository() }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.ERROR)  // requires (Level.ERROR) to not crash on phone
+            modules(koinModule)
+        }
+    }
 }
