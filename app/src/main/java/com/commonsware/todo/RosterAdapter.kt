5ec1067f1c272f685c1b4c4eb8f97930f3ea6818
neillbarrett <barrettneill@gmail.com>
2023-11-24 19:45:55 -0800

    Completed T16-Tracking the Completion Status

--- app/src/main/java/com/commonsware/todo/RosterAdapter.kt
+++ app/src/main/java/com/commonsware/todo/RosterAdapter.kt
@@ -7,12 +7,15 @@ import androidx.recyclerview.widget.ListAdapter
 import com.commonsware.todo.databinding.TodoRosterBinding
 import com.commonsware.todo.databinding.TodoRowBinding
 
-class RosterAdapter(private val inflater: LayoutInflater) :
+class RosterAdapter(
+    private val inflater: LayoutInflater,
+    private val onCheckboxToggle: (ToDoModel) -> Unit
+) :
     ListAdapter<ToDoModel, RosterRowHolder>(DiffCallback) {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ) = RosterRowHolder (TodoRowBinding.inflate(inflater, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        RosterRowHolder (
+            TodoRowBinding.inflate(inflater, parent, false),
+            onCheckboxToggle)
 
     override fun onBindViewHolder(holder: RosterRowHolder, position: Int) {
         holder.bind(getItem(position))
